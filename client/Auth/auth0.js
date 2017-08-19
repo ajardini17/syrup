a5f2898d6ab226ea5b798a2b3e56aa6e30bae682
ajardini17 <jardini.alex@gmail.com>
2017-08-19 10:08:27 -0700

    Fixed a slash

--- client/Auth/auth0.js
+++ client/Auth/auth0.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
     domain: 'austenwma.auth0.com',
     clientID: 'QhrHwNAodTcg3xBm47FoMBp2nSsnCXJA',
-    redirectUri: 'https://syrup.herokuapp.com//upload'
+    redirectUri: 'https://syrup.herokuapp.com/upload'
     
 }
 
