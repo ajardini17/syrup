52187f9f4802aad56ded95e9d8ddc8641b725eeb
ajardini17 <jardini.alex@gmail.com>
2017-08-19 13:04:15 -0700

    configuring reddis

--- db/redis.js
+++ db/redis.js
@@ -2,6 +2,10 @@ const redis = require('redis');
 
 const client = redis.createClient();
 
+var url = require('url');
+var redisURL = url.parse(process.env.REDIS_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
 // if you'd like to select database 3, instead of 0 (default), call 
 // client.select(3, function() { /* ... */ }); 
 
